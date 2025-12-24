a9deec5080ad0f2e1822224f5fe2cff99fa4b8ab
Hassan Bazzoun <xcode.bazzoun@gmail.com>
2025-12-24 20:33:19 +0200

    - added crud operation to books
    Signed-off-by: XcodeBn <86073708+xcodeBn@users.noreply.github.com>

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,9 +1,43 @@
-import { Controller } from '@nestjs/common';
-import { BooksService } from './books.service';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Book, BooksService } from './books.service';
 
 @Controller('books')
 export class BooksController {
   constructor(private readonly booksService: BooksService) {}
 
   // TODO: Implement book controller methods
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.booksService.findOne(+id);
+  }
+
+  @Get()
+  findAll() {
+    return this.booksService.findAll();
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.booksService.delete(+id);
+  }
+
+  @Post()
+  create(@Body() book: { title: string; authorId: string }) {
+    return this.booksService.create({
+      ...book,
+      authorId: +book.authorId,
+    });
+  }
+
+
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body() book: { title: string; authorId: string },
+  ) {
+    return this.booksService.update(+id, {
+      ...book,
+      authorId: +book.authorId,
+    });
+  }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,11 +1,86 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { AuthorsService } from '../authors/authors.service';
+
+export interface Book {
+  id: number;
+  title: string;
+  authorId: number;
+}
 
 @Injectable()
 export class BooksService {
-  private books = [
-    // TODO: add some sample books here
+  private books: Book[] = [
+    {
+      id: 1,
+      title: 'The Hobbit',
+      authorId: 1,
+    },
+    {
+      id: 2,
+      title: 'The Fellowship of the Ring',
+      authorId: 1,
+    },
+    {
+      id: 3,
+      title: 'The Two Towers',
+      authorId: 1,
+    },
+    {
+      id: 4,
+      title: 'The Return of the King',
+      authorId: 1,
+    },
+    {
+      id: 5,
+      title: 'The Colour of Magic',
+      authorId: 2,
+    },
+    {
+      id: 6,
+      title: 'The Light Fantastic',
+      authorId: 2,
+    },
   ];
 
   constructor(private readonly authorsService: AuthorsService) {}
+
+  findAll(): Book[] {
+    return this.books;
+  }
+
+  findOne(id: number): Book {
+    const book = this.books.find((book) => book.id === id);
+    if (!book) {
+      throw new NotFoundException(`Book with ID ${id} not found`);
+    }
+    return book;
+  }
+
+  delete(id: number) {
+    const book = this.books.find((book) => book.id === id);
+    if (!book) {
+      throw new NotFoundException(`Book with ID ${id} not found`);
+    }
+    this.books.splice(this.books.indexOf(book), 1);
+  }
+
+  create(book: { title: string; authorId: number }) {
+    const newBook = {
+      id: this.books.length + 1,
+      ...book,
+    };
+    this.books.push(newBook);
+  }
+
+  update(id: number, book: { title: string; authorId: number }) {
+    const bookToUpdateIndex = this.books.findIndex((book) => book.id === id);
+    if (!bookToUpdateIndex || bookToUpdateIndex == -1) {
+      throw new NotFoundException(`Book with ID ${id} not found`);
+    }
+
+    return (this.books[bookToUpdateIndex] = {
+      ...this.books[bookToUpdateIndex],
+      ...book,
+    });
+  }
 }
