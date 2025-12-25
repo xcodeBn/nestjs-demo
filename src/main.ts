35cd063c812ac3c9b15efb13620be5ad3bcdd24d
XcodeBn <86073708+xcodeBn@users.noreply.github.com>
2025-12-25 14:17:29 +0200

    -added swagger setup
    Signed-off-by: XcodeBn <86073708+xcodeBn@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  setupSwagger(app);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
+function setupSwagger(app: INestApplication<any>) {
+  const config = new DocumentBuilder()
+    .setTitle('Books Api')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
+
 bootstrap();
